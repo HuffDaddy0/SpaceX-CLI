b8d90b5eac41cc541294ae82d551611c7d14bb8b
HuffDaddy0 <ty.huff24@gmail.com>
2020-08-20 18:43:01 -0400

    first commit

---
+++ lib/Flatiron/SpaceX/CLI.rb
@@ -0,0 +1,10 @@
+require "Flatiron/SpaceX/CLI/version"
+
+module Flatiron
+  module SpaceX
+    module CLI
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Flatiron/SpaceX/CLI/version.rb
@@ -0,0 +1,7 @@
+module Flatiron
+  module SpaceX
+    module CLI
+      VERSION = "0.1.0"
+    end
+  end
+end
